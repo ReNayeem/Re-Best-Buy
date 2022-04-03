f16f97322d0f5dcacf20290b24dab851ce1cfb76
ReNayeem <renayeem@gmail.com>
2022-04-03 23:00:27 +0600

    product section done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <h2>Best buy</h2>
+            <h1 className='brand-name mt-3'>Best buy</h1>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/reviews">Reviews</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/blogs">Blogs</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/reviews">Reviews</NavLink>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+                <NavLink to="/blogs">Blogs</NavLink>
             </nav>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            {
-                <h1>Home</h1>
-            }
+        <div className='first-section mt-4'>
+            <div className='home-first-section container d-flex'>
+                <div>
+                    <h1 className='product-name-text'><span className='h1-text-highlight'>SONY - WH-XB900N</span> <span className='h1-text-highlight-2'>Wireless Noise Cancelling Over-the-Ear Headphones - Gray</span></h1>
+                    <br />
+                    <p className='product-detail'>Effectively blocks out distracting background noises, so you can enjoy a focused, immersive sound environment. Offers up to 30 hours of use and lasts up to 300 hours on standby mode. Enables simple wireless pairing with your Bluetooth-enabled device. Supports HFP, HSP, A2DP and AVRCP Bluetooth profiles. Deliver powerful sound. Along with a 50Hz - 8kHz microphone frequency response ensures faithful sound reproduction.</p>
+                    <button className="first-section-button">
+                        <span className="hover-underline-animation"> Click here </span>
+                        <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
+                            <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div>
+                    <img className='product-image' src="Resources/headphone.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
