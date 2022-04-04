3883b589af5b63087eb16d24900f0f7fcb2862fd
ReNayeem <renayeem@gmail.com>
2022-04-04 13:23:22 +0600

    not found completed

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const notFound = () => {
     return (
-        <div>
-            <h2>The page you are looking for is not available</h2>
-            <h4>404</h4>
+        <div className='not-found'>
+            <h1 className='h1-404'>404!</h1>
+            <h2 className='h2-404'>The page you are looking for is not found</h2>
         </div>
     );
 };
